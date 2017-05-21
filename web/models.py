3c62fabef4336c8637bb7ce1d7ad02b6c5c36ebc
t005 <t005@mail.nksh.tp.edu.tw>
2017-05-21 02:54:55 -0400

    first commit

---
+++ web/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Message(models.Model):
+        user = models.CharField(max_length=50)
+        subject = models.CharField(max_length=200)
+        publication_date = models.DateTimeField()
+        
+        def __unicode__(self):
+                return self.subject
